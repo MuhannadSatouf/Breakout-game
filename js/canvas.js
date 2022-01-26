63face21df1d1b911d4a18b1177ecb682a64e6fd
MuhannadSatouf <muhannad.satouf0016@stud.hkr.se>
2022-01-26 13:30:05 +0100

    Move and bouncing the ball

--- js/canvas.js
+++ js/canvas.js
@@ -1,14 +1,17 @@
 //Create My Canvas that will contains everything
-var canvas = document.getElementById("myCanvas");
+let canvas = document.getElementById("myCanvas");
 
 //Create 2D variables
-var canvasContext = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
+let canvasContext = canvas.getContext("2d");
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let newPositionX = 2;
+let newPositionY = -2;
+let ballRadius = 5;
 
 function drawBall() {
   canvasContext.beginPath();
-  canvasContext.arc(x, y, 10, 0, Math.PI * 2);
+  canvasContext.arc(x, y, ballRadius, 0, Math.PI * 2);
   canvasContext.fillStyle = "#0095DD";
   canvasContext.fill();
   canvasContext.closePath();
@@ -17,6 +20,25 @@ function drawBall() {
 function draw() {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  checkWall();
 }
 
+function checkWall() {
+  if (
+    x + newPositionX < ballRadius ||
+    x + newPositionX > canvas.width - ballRadius
+  ) {
+    newPositionX = -newPositionX;
+  }
+  if (
+    y + newPositionY > canvas.height - ballRadius ||
+    y + newPositionY < ballRadius
+  ) {
+    newPositionY = -newPositionY;
+  }
+
+  x += newPositionX;
+  y += newPositionY;
+}
+//For continues drawing
 setInterval(draw, 10);
